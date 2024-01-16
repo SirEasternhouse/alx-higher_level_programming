adaee210259567150b7114dc9e65cbfea4bbca82
SirEasternhouse <aso21arnold.oosthuizen@gmail.com>
2024-01-16 14:23:43 -0800

    fix print statement

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 for char in range(ord('a'), ord('z') + 1):
-    print("{}".format(chr(char)), end='\n' if char == ord('z') else '')
+    print("{}".format(chr(char)), end='\n' if char == ord('z') else'')
