256e77b0413383af3d53d45ad519d7ed017d49ea
SirEasternhouse <aso21arnold.oosthuizen@gmail.com>
2024-01-17 15:26:16 -0800

    remove char

---
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def remove_char_at(str, n):
+    if 0 <= n < len(str):
+        result = ''
+        for i in range(len(str)):
+            if i != n:
+                result += str[i]
+        return result
+    else:
+        return str
