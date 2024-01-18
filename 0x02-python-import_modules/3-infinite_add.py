e8f6004144f5bb6a0640c4a2e37ae54b4d469a48
SirEasternhouse <aso21arnold.oosthuizen@gmail.com>
2024-01-18 15:41:32 -0800

    add args

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,1 +1,18 @@
 #!/usr/bin/python3
+
+
+import sys
+
+
+def add_arguments(*args):
+    return sum(map(int, args))
+
+
+if __name__ == "__main__":
+    arguments = sys.argv[1:]
+
+    if arguments:
+        result = add_arguments(*arguments)
+        print(result)
+    else:
+        print("No arguments provided")
