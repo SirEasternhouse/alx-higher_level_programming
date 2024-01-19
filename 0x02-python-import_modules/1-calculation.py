4bfe945f015d7235371ef5519c568311c1521076
SirEasternhouse <aso21arnold.oosthuizen@gmail.com>
2024-01-19 10:53:50 -0800

    completion of code

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+from calculator_1 import add, sub, div, mul
+
+if __name__ == "__main__":
+    a = 10
+    b = 5
+
+    c = add(a, b)
+    d = sub(a, b)
+    e = mul(a, b)
+    f = div(a, b)
+
+    print("{} + {} = {}".format(a, b, c))
+    print("{} - {} = {}".format(a, b, d))
+    print("{} * {} = {}".format(a, b, e))
+    print("{} / {} = {}".format(a, b, f))
