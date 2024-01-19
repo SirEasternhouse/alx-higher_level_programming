5319372b32ea15f7d099970f4cf7ccbcff7f8411
SirEasternhouse <aso21arnold.oosthuizen@gmail.com>
2024-01-19 12:19:09 -0800

    print args

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+import sys
+
+if __name__ == "__main__":
+    num_args = len(sys.argv) - 1
+
+    if num_args == 0:
+        print("0 arguments.")
+    else:
+        print("{} argument{}:".format(num_args, "" if num_args == 1 else "s"))
+        for i, arg in enumerate(sys.argv[1:]):
+            print("{}: {}".format(i + 1, arg))
