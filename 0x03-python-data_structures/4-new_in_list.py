8743ff99e15a37a2e4e145f3bbab4c4452229a8b
SirEasternhouse <aso21arnold.oosthuizen@gmail.com>
2024-01-22 15:42:31 -0800

    modify copy list

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,2 +1,9 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
+    copy_list = list.copy(my_list)  # creating a copy of orgiginal list
+
+    if idx < 0 or idx > len(copy_list):  # out of range and negative idx
+        return copy_list
+    else:
+        copy_list[idx] = element  # modified copy
+        return copy_list
