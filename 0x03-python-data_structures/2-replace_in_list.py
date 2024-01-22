366957cbe1d6382f7182e38e4ecd3791e2a5198d
SirEasternhouse <aso21arnold.oosthuizen@gmail.com>
2024-01-22 14:51:20 -0800

    correction of pycodestyle

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,8 +1,8 @@
 #!/bin/usr/python3
 def replace_in_list(my_list, idx, element):
-    if idx < 0: #  negative idx to return original list
+    if idx < 0:  # negative idx to return original list
         return my_list
-    elif idx > len(my_list): # idx out of range return original list
+    elif idx > len(my_list):  # idx out of range return original list
         return my_list
     else:  # modified list with new element
         my_list[idx] = element
