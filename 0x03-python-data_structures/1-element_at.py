4c765b60fa7b82158de33c52cb8975676a8bed15
SirEasternhouse <aso21arnold.oosthuizen@gmail.com>
2024-01-22 14:00:14 -0800

    retrieve list element

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,2 +1,8 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
+    if idx < 0:  # checking for negative index
+        return None
+    elif idx > len(my_list):  # checking for out of range
+        return None
+    else:
+        return my_list[idx]
