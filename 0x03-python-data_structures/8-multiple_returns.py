80295e8776a3500d048f5bec08bb2313fa946857
SirEasternhouse <aso21arnold.oosthuizen@gmail.com>
2024-01-23 13:42:32 -0800

    multi returns

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,2 +1,10 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
+    if sentence:
+        length = len(sentence)
+        first_char = sentence[0]
+    else:
+        lenght = 0
+        first_char = None
+
+    return length, first_char
