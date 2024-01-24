d2c67ca446e97e5c025093f3e53223a6b5c4b76a
SirEasternhouse <aso21arnold.oosthuizen@gmail.com>
2024-01-24 15:25:29 -0800

    search replace

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,2 +1,9 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
+    replace_list = []
+    for i in my_list:
+        if search == i:
+            replace_list.append(replace)
+        else:
+            replace_list.append(i)
+    return replace_list
