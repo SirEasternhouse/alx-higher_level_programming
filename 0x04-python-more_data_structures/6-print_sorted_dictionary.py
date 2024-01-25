6e91bd46ff3332afe438920a1d94dee2cf64f2fe
SirEasternhouse <aso21arnold.oosthuizen@gmail.com>
2024-01-25 03:02:21 -0800

    sort dict

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,2 +1,9 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
+    for key in sorted(a_dictionary.keys()):
+        value = a_dictionary[key]
+        if isinstance(value, dict):
+            print("{}:".format(key))
+            print_sorted_dictionary(value)
+        else:
+            print("{}: {}".format(key, value))
