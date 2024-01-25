605870f924b76c9fba369b85835337976922131a
SirEasternhouse <aso21arnold.oosthuizen@gmail.com>
2024-01-25 04:43:57 -0800

    find best score

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,2 +1,12 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
+    if not a_dictionary:
+        return None
+
+    best_key = None
+    best_value = float('-inf')
+
+    for key, value in a_dictionary.items():
+        if value > best_value:
+            best_key, best_value = key, value
+    return best_key
