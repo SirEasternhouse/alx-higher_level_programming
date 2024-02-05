4d9ffd411a27a23f7e3c4ea7a4e9d6e00d55a56c
SirEasternhouse <aso21arnold.oosthuizen@gmail.com>
2024-02-05 14:19:19 -0800

    division

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,2 +1,14 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
+    try:
+        result = a / b
+    except ZeroDivisionError:
+        print("Inside result: None")
+        return None
+    except TypeError:
+        print("Inside result: None")
+        return None
+    else:
+        print("Inside result: {}".format(result))
+    finally:
+        return result if 'result' in locals() else None
