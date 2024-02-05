e3887c7c4dd7dea51262648d902a7a90de26fa40
SirEasternhouse <aso21arnold.oosthuizen@gmail.com>
2024-02-05 13:58:01 -0800

    index error fix

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -5,12 +5,12 @@ def safe_print_list_integers(my_list=[], x=0):
         for i in range(x):
             value = my_list[i]
             try:
-                print("{:d}".format(value), end=" ")
+                print("{:d}".format(value), end="")
                 count += 1
             except (ValueError, TypeError):
                 pass
     except IndexError:
-        pass
+       raise
 
     print()
     return count
