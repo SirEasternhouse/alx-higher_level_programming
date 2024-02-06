c4d76b62af824cbcccb32c23ec4c28cb1dc534d4
SirEasternhouse <aso21arnold.oosthuizen@gmail.com>
2024-02-06 13:41:16 -0800

    rev doc-string

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,9 +1,4 @@
 #!/usr/bin/python3
 class Square:
-    """
-    A class that represents a square.
-
-    This class can be used to create square objects
-    """
-
+    """This class can be used to create square objects"""
     pass
