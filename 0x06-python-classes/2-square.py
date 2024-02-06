d6ace6f84fde01862b3ce1214f19c56dc02d70bc
SirEasternhouse <aso21arnold.oosthuizen@gmail.com>
2024-02-06 14:24:28 -0800

    square exception

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,3 +1,17 @@
 #!/usr/bin/python3
-class Square:
+"""This class can be used to create square objects"""
 
+
+class Square:
+    """defining a class square with a size attirbute"""
+    def __init__(self, size=0):
+        """ parameters:
+            param 1: size
+                        the size of the square starting at 0
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size mus be >= 0")
+        else:
+            self.__size = size
