9728275594266812526fb8bab22d00a4b46a7860
SirEasternhouse <aso21arnold.oosthuizen@gmail.com>
2024-02-06 13:11:06 -0800

    OOP

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+class square:
+    """ Defining a Square class to determine area"""
+    pass
---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+class Square:
+    "defining a class square with a size attirbute"
+
