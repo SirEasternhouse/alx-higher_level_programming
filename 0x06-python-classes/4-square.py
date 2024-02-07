a175014398f7de5dec55cb2239fcea9dd4ec5a43
SirEasternhouse <aso21arnold.oosthuizen@gmail.com>
2024-02-07 12:51:58 -0800

    task4

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,2 +1,34 @@
-#!/usr/bin/python3:
+#!/usr/bin/python3
+"""This class can be used to create square objects"""
+
+
 class Square:
+    """defining a class square with a size attirbute"""
+
+    def __init__(self, size=0):
+        """ parameters:
+            param 1: size
+                the size of the square starting at 0
+        """
+        self.__size = size
+
+    @property
+    def size(self):
+        """retrieving the attribute value of size"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """value: checking the integer validity of size"""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    """calculating the area of the square"""
+
+    def area(self):
+        """ Returns the area of a square """
+        return self.__size ** 2
