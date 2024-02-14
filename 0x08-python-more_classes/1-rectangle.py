b8c1c699d481ae2a3998ad2a7575deac0334f06b
SirEasternhouse <aso21arnold.oosthuizen@gmail.com>
2024-02-14 11:45:27 -0800

    fix

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -3,23 +3,21 @@ class Rectangle:
     """
     Represents a rectangle with a width and height.
 
-    Attributes:
-        width (int): The width of the rectangle.
-        height (int): The height of the rectangle.
     """
 
     def __init__(self, width=0, height=0):
-    """
-    Initializes a new Rectangle object.
-
-    Args:
-        width (int, optional): The width of the rectangle. Defaults to 0.
-        height (int, optional): The height of the rectangle. Defaults to 0.
-
-    Raises:
-        TypeError: If either width or height is not an integer.
-        ValueError: If either width or height is less than 0.
         """
+        Initializes a new Rectangle object.
+
+        Args:
+            width (int, optional): The width of the rectangle. Defaults to 0.
+            height (int, optional): The height of the rectangle. Defaults to 0.
+
+        Raises:
+            TypeError: If either width or height is not an integer.
+            ValueError: If either width or height is less than 0.
+        """
+
         self._validate_width(width)
         self._validate_height(height)
 
@@ -28,81 +26,81 @@ class Rectangle:
 
     @property
     def width(self):
-    """
-    Gets the width of the rectangle.
+        """
+        Gets the width of the rectangle.
 
-    Returns:
-        int: The width of the rectangle.
-    """
+        Returns:
+            int: The width of the rectangle.
+        """
         return self.__width
 
     @width.setter
     def width(self, value):
-    """
-    Sets the width of the rectangle.
+        """
+        Sets the width of the rectangle.
 
-    Args:
-        value (int): The new width of the rectangle.
+        Args:
+            value (int): The new width of the rectangle.
 
-    Raises:
-        TypeError: If value is not an integer.
-        ValueError: If value is less than 0.
-    """
+        Raises:
+            TypeError: If value is not an integer.
+            ValueError: If value is less than 0.
+        """
         self._validate_width(value)
         self.__width = value
 
     @property
     def height(self):
-    """
-    Gets the height of the rectangle.
+        """
+        Gets the height of the rectangle.
 
-    Returns:
-        int: The height of the rectangle.
-    """
+        Returns:
+            int: The height of the rectangle.
+        """
         return self.__height
 
     @height.setter
     def height(self, value):
-    """
-    Sets the height of the rectangle.
+        """
+        Sets the height of the rectangle.
 
-    Args:
-        value (int): The new height of the rectangle.
+        Args:
+            value (int): The new height of the rectangle.
 
-    Raises:
-        TypeError: If value is not an integer.
-        ValueError: If value is less than 0.
-    """
+        Raises:
+            TypeError: If value is not an integer.
+            ValueError: If value is less than 0.
+        """
         self._validate_height(value)
         self.__height = value
 
     def _validate_width(self, value):
-    """
-    Validates the width value.
+        """
+        Validates the width value.
 
-    Args:
-        value: The value to be validated.
+        Args:
+            value: The value to be validated.
 
-    Raises:
-        TypeError: If value is not an integer.
-        ValueError: If value is less than 0.
-    """
+        Raises:
+            TypeError: If value is not an integer.
+            ValueError: If value is less than 0.
+        """
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
 
     def _validate_height(self, value):
-    """
-    Validates the height value.
+        """
+        Validates the height value.
 
-    Args:
-         value: The value to be validated.
+        Args:
+            value: The value to be validated.
 
-    Raises:
-        TypeError: If value is not an integer.
-        ValueError: If value is less than 0.
-    """
+        Raises:
+            TypeError: If value is not an integer.
+            ValueError: If value is less than 0.
+        """
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
