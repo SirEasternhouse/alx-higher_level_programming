184d54d3c5cef2e5d78615468c5cc127c6e2f5f6
SirEasternhouse <aso21arnold.oosthuizen@gmail.com>
2024-02-15 14:07:34 -0800

    task 1: documentation

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,5 +1,13 @@
 #!/usr/bin/python3
 def add_integer(a, b=98):
+    """Add two intergers where b is set to a default number
+    Args:
+        param1: input and integer or float
+        param2: default set to 98 , yet an interger or float may be inputed
+
+    Return:
+        the addtion of integer a and b
+    """
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
 
