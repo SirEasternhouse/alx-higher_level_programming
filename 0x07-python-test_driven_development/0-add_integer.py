f039fa2940cd25f14106ec9e1f461ee331bd69c9
SirEasternhouse <aso21arnold.oosthuizen@gmail.com>
2024-02-15 14:32:44 -0800

    accounting for none cases

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -8,6 +8,8 @@ def add_integer(a, b=98):
     Return:
         the addtion of integer a and b
     """
+    if a is None:
+        raise TypeError("a must be an integer")
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
 
