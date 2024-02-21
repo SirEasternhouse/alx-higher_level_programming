0057d3e0ac33a856b2d2d8171e6aff9509e51bdd
SirEasternhouse <aso21arnold.oosthuizen@gmail.com>
2024-02-21 15:31:18 -0800

    pycodestyle & document

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,22 +1,17 @@
 #!/usr/bin/python3
+"""Writes a string to a text file and returns the number of chars written."""
+
+
 def write_file(filename="", text=""):
-    """Writes a string to a text file and returns the number of characters written.
+    """Writes a string to a text file and returns the number of chars written.
 
     Args:
-        filename (str): The name of the file to write to. Default is an empty string.
-        text (str): The string to write to the file. Default is an empty string.
+        filename (str): The name of the file to write to.
+        text (str): The string to write to the file.
 
     Returns:
         int: The number of characters written to the file.
 
-    Note:
-        - This function overwrites the content of the file if it already exists.
-        - If the file does not exist, it will be created.
-        - File permissions are not managed by this function.
-
-    Example:
-        >>> write_file("example.txt", "Hello, world!")
-        13
     """
     try:
         with open(filename, "w", encoding="utf-8") as file:
