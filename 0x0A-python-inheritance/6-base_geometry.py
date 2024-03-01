24186744c8054aded241340236f968d47d862911
SirEasternhouse <aso21arnold.oosthuizen@gmail.com>
2024-03-01 14:37:58 -0800

    improve geometry class

--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -1,8 +1,20 @@
 #!/usr/bin/python3
+""" Base class for geometry operations."""
+
+
 class BaseGeometry:
     """
-        a base geometry class
+    A base class for geometry operations.
+
+    Public Methods:
+    area(): Raises an Exception with the message "area() is not implemented".
     """
 
-
     def area(self):
+        """
+        Raises an Exception indicating that the method is not implemented.
+
+        Raises:
+        Exception: Indicates that the method is not implemented.
+        """
+        raise Exception("area() is not implemented")
