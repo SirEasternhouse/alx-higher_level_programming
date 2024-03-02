198925aae696bf502347245195e07df9a37a65c6
SirEasternhouse <aso21arnold.oosthuizen@gmail.com>
2024-03-02 13:02:00 -0800

    revision of tests

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -1,43 +1,38 @@
 #!/usr/bin/python3
-""" unit test for  max-integer"""
+"""Unittest for max_integer([..])
+"""
 import unittest
-
-
 max_integer = __import__('6-max_integer').max_integer
 
 
 class TestMaxInteger(unittest.TestCase):
     """
-        testint the max integer in a list
+    Test cases for the max_integer function.
     """
 
-    def test_max_integer(self):
+    def test_max_integer_positive(self):
         """
-            test cases for finding max value in list
+        Test max_integer with a list of positive integers.
         """
-        self.assertEqual(max_integer([1, 3, 5, 2]), 5)
-        self.assertEqual(max_integer([-1, -3, -5, -2]), -1)
-        self.assertEqual(max_integer([10]), 10)
-        self.assertEqual(max_integer([0, 0, 0]), 0)
+        self.assertEqual(max_integer([1, 2, 3, 4, 5]), 5)
 
-    def test_empty_list(self):
+    def test_max_integer_negative(self):
         """
-            test  for an empty list
+        Test max_integer with a list of negative integers.
+        """
+        self.assertEqual(max_integer([-1, -2, -3, -4, -5]), -1)
+
+    def test_max_integer_mixed(self):
+        """
+        Test max_integer with a list of mixed positive and negative integers.
+        """
+        self.assertEqual(max_integer([5, -2, 7, -4, 9]), 9)
+
+    def test_max_integer_empty_list(self):
+        """
+        Test max_integer with an empty list.
         """
         self.assertIsNone(max_integer([]))
-
-    def test_invalid_input(self):
-        """
-            Test case: list contains non-integer elements
-        """
-        with self.assertRaises(TypeError):
-            max_integer(['a', 'b', 'c'])
-        with self.assertRaises(TypeError):
-            max_integer([1, 2, 'c'])
-        with self.assertRaises(TypeError):
-            max_integer([1, 2, None])
-        with self.assertRaises(TypeError):
-            max_integer([1, 2, {}])
 
 
 if __name__ == '__main__':
