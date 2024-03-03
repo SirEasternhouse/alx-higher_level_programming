60e13c438aeb37775169a6304dd95efa70071d8a
SirEasternhouse <aso21arnold.oosthuizen@gmail.com>
2024-03-03 10:29:51 -0800

    pascal tri

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+""" Generate Pascal's triangle up to the nth row."""
+
+def pascal_triangle(n):
+    """
+
+    Args:
+        n (int): The number of rows to generate.
+
+    Returns:
+        list of lists: A list of lists representing Pascal's triangle.
+
+    """
+    if n <= 0:
+        return []
+
+    triangle = [[1]]
+
+    for i in range(1, n):
+        row = [1]
+        prev_row = triangle[-1]
+        for j in range(1, i):
+            row.append(prev_row[j - 1] + prev_row[j])
+        row.append(1)
+        triangle.append(row)
+
+    return triangle
