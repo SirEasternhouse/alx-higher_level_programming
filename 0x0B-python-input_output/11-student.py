e1ba32757f367bf0f25da01d448e5678bc10ce97
SirEasternhouse <aso21arnold.oosthuizen@gmail.com>
2024-03-03 10:13:55 -0800

    improve student class 2

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+""" Class representing a student. """
+
+
+class Student:
+    """
+    Attributes:
+        first_name (str): The first name of the student.
+        last_name (str): The last name of the student.
+        age (int): The age of the student.
+    """
+
+    def __init__(self, first_name, last_name, age):
+        """
+        Initialize a Student instance with first name, last name, and age.
+
+        Args:
+            first_name (str): The first name of the student.
+            last_name (str): The last name of the student.
+            age (int): The age of the student.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        Retrieve a dictionary representation of the Student instance.
+
+        Args:
+            attrs (list of str, optional): List of attribute names to retrieve.
+            If None, retrieve all attributes.
+
+        Returns:
+            dict: A dictionary representation of the Student instance.
+        """
+        if attrs is None:
+            return {
+                "first_name": self.first_name,
+                "last_name": self.last_name,
+                "age": self.age
+            }
+        else:
+            return {attr: getattr(self, attr) for attr in attrs}
+
+    def reload_from_json(self, json):
+        """
+        Replace all attributes of the Student instance using a dictionary.
+
+        Args:
+            json (dict): A dictionary containing attribute names and their corresponding values.
+        """
+        for key, value in json.items():
+            setattr(self, key, value)
