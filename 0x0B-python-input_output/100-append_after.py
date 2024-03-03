c6767863c3d71833e61fc009a0fa183db9d4951d
SirEasternhouse <aso21arnold.oosthuizen@gmail.com>
2024-03-03 10:47:32 -0800

    append txt after

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+def append_after(filename="", search_string="", new_string=""):
+    """
+    Inserts a line of text after each line containing
+    a specific string in a file.
+
+    Args:
+        filename (str): The name of the file.
+        search_string (str): The string to search for in each line.
+        new_string (str): The str to insrt after each line cont the search str.
+    """
+    with open(filename, 'r') as file:
+        lines = file.readlines()
+
+    with open(filename, 'w') as file:
+        for line in lines:
+            file.write(line)
+            if search_string in line:
+                file.write(new_string + '\n')
