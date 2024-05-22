15a8c2cd5517c139aad1cce1563a8b8b0d07fe29
SirEasternhouse <aso21arnold.oosthuizen@gmail.com>
2024-05-22 15:07:52 -0700

    sql join

---
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,11 @@
+-- lists all cities contained in the database hbtn_0d_usa using join
+SELECT 
+	cities.id,
+	cities.name,
+	states.name -- AS state_name
+FROM
+	cities
+INNER JOIN
+	states ON cities.state_id = states.id
+ORDER BY 
+	cities.id ASC;
