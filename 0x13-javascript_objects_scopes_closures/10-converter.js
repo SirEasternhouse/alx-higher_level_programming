1dbd6ce8d27a4f0d45e83b5626c7f27b4da8af81
SirEasternhouse <aso21arnold.oosthuizen@gmail.com>
2024-06-19 12:12:44 -0700

    refactor code

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -3,13 +3,7 @@
 exports.converter = function (base) {
   if (base <= 0 || base > 36) return;
 
-  const convertRecursive = function (number) {
-    if (number < base) {
-      return number.toString(36).toUpperCase();
-    } else {
-      return convertRecursive(parseInt(number / base)) + (number % base).toString(36).toUpperCase();
-    }
+  return function convertRecursive (number) {
+    return (number < base) ? number.toString(36).toUpperCase() : convertRecursive(parseInt(number / base)) + (number % base).toString(36).toUpperCase();
   };
-
-  return convertRecursive;
 };
