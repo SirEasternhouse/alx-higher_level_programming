caf653cfb9e2649be809a124b8a0573c363951e2
SirEAsternhouse <aso21arnold.oosthuizen@gmail.com>
2024-07-05 13:53:15 -0700

    header response value

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Takes in a URL, sends a request to the URL, and
+displays the value of the X-Request-Id variable in the response header
+"""
+
+import requests
+import sys
+
+
+def fetch_x_request_id(url):
+    """Fetches the X-Request-Id from the response header of the URL"""
+    response = requests.get(url)
+    x_request_id = response.headers.get('X-Request-Id')
+    print(x_request_id)
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    fetch_x_request_id(url)
